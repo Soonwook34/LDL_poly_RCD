f75da6df519484c2b80b1d60c9c69386c0414f02
soonwook34 <soonwook34@gmail.com>
2022-09-16 11:28:03 +0000

    test generation

--- RCD/data/gendata/generate_dataset_KT.py
+++ RCD/data/gendata/generate_dataset_KT.py
@@ -2,7 +2,7 @@ import numpy as np
 import argparse
 import json
 import random
-from scipy.stats import norm
+from scipy.stats import norm, bernoulli
 
 
 class GenDataArgParser(argparse.ArgumentParser):
@@ -18,14 +18,12 @@ class GenDataArgParser(argparse.ArgumentParser):
                           help='Shape of concept map / 0: line, 1: binary tree')
         self.add_argument('--sample_n', type=int, default=100,
                           help='Number of sampling')
-        self.add_argument('--ability_min', type=int, default=0.2,
-                          help='Minimum initial answer rate(ability), [0,1)')
         self.add_argument('--name', type=str, default='test',
                           help='Dataset name')
         self.add_argument('--ability_min', type=float, default=0,
-                          help="Min vaule of student's ability")
-        self.add_argument('--ability_max', type=float, default=5,
-                          help="Max vaule of student's ability")
+                          help="Min vaule of student's initial ability, [0,1)")
+        self.add_argument('--ability_max', type=float, default=1,
+                          help="Max vaule of student's initial ability, (args.ability_min, 1]")
         self.add_argument('--difficulty_min', type=float, default=0,
                           help="Min vaule of exercise's difficulty")
         self.add_argument('--difficulty_max', type=float, default=5,
@@ -41,33 +39,36 @@ class GenDataArgParser(argparse.ArgumentParser):
 
 
 class Student():
-    def __init__(self, args, exercise_per_concept):
-        # 학생 응답 기록 a[s][e] (right or wrong)
+    def __init__(self, args, s, exercise_per_concept):
+        self.num = s
+        # 학생 응답 기록 a_s,e (right or wrong)
         self.responses = [[0 for e in range(exercise_per_concept)] for c in range(args.concept_n)]
-        # 학생 정답 기록, 1[a[s][e] == 1(right)]
+        # 학생 정답 기록, 1[a_{s,e == 1(right)]
         self.answers = [[0 for e in range(exercise_per_concept)] for c in range(args.concept_n)]
         # 학생 concept별 정답률(=능력) 기록, p(c)
         self.abilities = [0 for c in range(args.concept_n)]
 
-    def calc_ability(self, concept):
-        self.abilities[concept] = np.mean(self.answers[concept])
+    def calc_ability(self, c):
+        self.abilities[c] = np.mean(self.answers[c])
 
 
 class Exercise():
     def __init__(self, args, e, c):
         self.num = e
-        self.concept = concept
+        self.c = c
         self.difficulty = np.random.rand() * (args.difficulty_max - args.difficulty_min) + args.difficulty_min
         self.discrimination = np.random.rand() * (args.discrimination_max - args.discrimination_min) + args.discrimination_min
         self.pseudo_guessing = np.random.rand() * (args.pseudo_guessing_max - args.pseudo_guessing_min) + args.pseudo_guessing_min
 
     def ICC(self, ability):
-        return max((1 - self.pseudo_guessing) / (1 + np.exp(-self.discrimination * (ability - self.difficulty))), 0)
+        ability = ability * 4
+        return min(max((1 - self.pseudo_guessing) / (1 + np.exp(-self.discrimination * (ability - self.difficulty))), 0), 1)
 
 
 class Concept():
     def __init__(self, args, c):
         self.num = c
+        self.sample_n = args.sample_n
         # prerequisite concept of concept c, c_pre
         # 일자 구조
         if args.concept_map == "0":
@@ -77,24 +78,37 @@ class Concept():
             self.c_pre = int((c - 1) / 2)
         self.c_pre = int((c - 1) / 2)
 
-    def sample_ability(self, ability_pre, sample_n):
-        return norm(ability_pre, 1).rvs(100).mean()
+    def sample_ability(self, student):
+        # print(student.abilities[self.c_pre])
+        return min(max(norm(student.abilities[self.c_pre], 0.1).rvs(self.sample_n).mean(), 0), 1)
 
 
 def generate_dataset(args):
     dataset = []
     exercise_per_concept = int(args.exercise_n / args.concept_n)
     students = [Student(args, s, exercise_per_concept) for s in range(args.student_n)]
-    exercises = [[Exercise(args, e + exercise_per_concept * c, c) for e in range(exercise_per_concept)] for c in range(args.concept_n)]
+    exercises = [[Exercise(args, e, c) for e in range(exercise_per_concept)] for c in range(args.concept_n)]
     concepts = [Concept(args, c) for c in range(args.concept_n)]
 
-    for s in students:
-        s.abilities[0] = random.uniform(args.ability_min, 1)
-        for c in concepts:
-            s.abilities[c.num] = c.sample_ability(s.abilities[c.c_pre], args.sample_n)
-            for e in exercises[c.num]:
-                # TODO: generate log
-    return dataset
+    ab = []
+    for student in students:
+        student.abilities[0] = random.uniform(args.ability_min, args.ability_max)
+        for concept in concepts:
+            p_c = concept.sample_ability(student)
+            # print(concept.num, concept.c_pre, p_c)
+            for exercise in exercises[concept.num]:
+                p_e_given_c = exercise.ICC(p_c)
+                # print(f"p(e|c) = {p_e_given_c}")
+                a_se = bernoulli.rvs(p_e_given_c)
+                # print(f"a_se = {a_se}")
+                student.responses[concept.num][exercise.num] = a_se
+                student.answers[concept.num][exercise.num] = a_se
+            student.calc_ability(concept.num)
+            ab.append(student.abilities[concept.num])
+            # print(f"p(c_{concept.num}): {student.abilities[concept.num]}({student.abilities[concept.c_pre]})")
+    # print(sum(ab) / len(ab))
+    return sum(ab) / len(ab)
+    # return dataset
 
 
 def convert_dataset(dataset):
@@ -106,7 +120,12 @@ def convert_dataset(dataset):
 if __name__ == '__main__':
     args = GenDataArgParser().parse_args()
     print(str(args))
-    dataset = generate_dataset(args)
-    log_data = convert_dataset(dataset)
-    with open(f"./log_data_{args.name}.json", 'w', encoding='utf8') as output_file:
-        json.dump(log_data, output_file, indent=4, ensure_ascii=False)
+    ab = []
+    for i in range(100):
+        print(f"{i}...")
+        a = generate_dataset(args)
+        ab.append(a)
+    print(sum(ab) / len(ab))
+    # log_data = convert_dataset(dataset)
+    # with open(f"./log_data_{args.name}.json", 'w', encoding='utf8') as output_file:
+    #     json.dump(log_data, output_file, indent=4, ensure_ascii=False)
