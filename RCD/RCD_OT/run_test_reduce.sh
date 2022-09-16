3b3cbff51bcb8b7f6b2dc53f3a2e126696e9d78f
soonwook34 <soonwook34@gmail.com>
2022-09-16 01:07:47 +0000

    update reduce/reduce_shuffle train/test sheel script

--- RCD/RCD_OT/run_test_reduce.sh
+++ RCD/RCD_OT/run_test_reduce.sh
@@ -1,17 +1,47 @@
-python test.py --gpu 0 --mode 0 --log KT_1_reduce
-python test.py --gpu 0 --mode 0 --log KT_edge_1_reduce
-sleep 5
-python test.py --gpu 2 --mode 1 --log KTOT_0.8_1_reduce
-python test.py --gpu 2 --mode 1 --log KTOT_0.8_edge_1_reduce
-sleep 5
-python test.py --gpu 1 --mode 1 --log KTOT_0.5_1_reduce
-python test.py --gpu 1 --mode 1 --log KTOT_0.5_edge_1_reduce
-sleep 5
-python test.py --gpu 0 --mode 1 --log KTOT_0.2_1_reduce
-python test.py --gpu 0 --mode 1 --log KTOT_0.2_edge_1_reduce
-sleep 5
-python test.py --gpu 1 --mode 1 --log KTOT_0.1_1_reduce
-python test.py --gpu 1 --mode 1 --log KTOT_0.1_edge_1_reduce
-sleep 5
-python test.py --gpu 2 --mode 2 --log OT_1_reduce
-python test.py --gpu 2 --mode 2 --log OT_edge_1_reduce
+cp config_full.txt config.txt
+cp ../data/poly/log_data_reduce.json ../data/poly/log_data.json
+cp ../data/poly/train_set_reduce.json ../data/poly/train_set.json
+cp ../data/poly/valid_set_reduce.json ../data/poly/valid_set.json
+cp ../data/poly/test_set_reduce.json ../data/poly/test_set.json
+
+python build_k_e_graph.py
+python build_u_e_graph.py
+
+nohup python -u test.py --gpu 0 --mode 0 --log_KT_reduce > /dev/null &
+nohup python -u test.py --gpu 1 --mode 1 --log_KTOT_0.1_reduce > /dev/null &
+python -u test.py --gpu 2 --mode 1 --log_KTOT_0.2_reduce
+sleep 10
+
+nohup python -u test.py --gpu 0 --mode 1 --log_KTOT_0.5_reduce > /dev/null &
+nohup python -u test.py --gpu 1 --mode 1 --log_KTOT_0.8_reduce > /dev/null &
+python -u test.py --gpu 2 --mode 2 --log_OT_reduce
+sleep 10
+
+nohup python -u test.py --gpu 0 --mode 0 --log_KT_edge_0_reduce > /dev/null &
+nohup python -u test.py --gpu 1 --mode 0 --log_KT_edge_1_reduce > /dev/null &
+python -u test.py --gpu 2 --mode 0 --log_KT_edge_2_reduce
+sleep 10
+
+nohup python -u test.py --gpu 0 --mode 0 --log_KTOT_0.1_edge_0_reduce > /dev/null &
+nohup python -u test.py --gpu 1 --mode 0 --log_KTOT_0.1_edge_1_reduce > /dev/null &
+python -u test.py --gpu 2 --mode 0 --log_KTOT_0.1_edge_2_reduce
+sleep 10
+
+nohup python -u test.py --gpu 0 --mode 0 --log_KTOT_0.2_edge_0_reduce > /dev/null &
+nohup python -u test.py --gpu 1 --mode 0 --log_KTOT_0.2_edge_1_reduce > /dev/null &
+python -u test.py --gpu 2 --mode 0 --log_KTOT_0.2_edge_2_reduce
+sleep 10
+
+nohup python -u test.py --gpu 0 --mode 0 --log_KTOT_0.5_edge_0_reduce > /dev/null &
+nohup python -u test.py --gpu 1 --mode 0 --log_KTOT_0.5_edge_1_reduce > /dev/null &
+python -u test.py --gpu 2 --mode 0 --log_KTOT_0.5_edge_2_reduce
+sleep 10
+
+nohup python -u test.py --gpu 0 --mode 0 --log_KTOT_0.8_edge_0_reduce > /dev/null &
+nohup python -u test.py --gpu 1 --mode 0 --log_KTOT_0.8_edge_1_reduce > /dev/null &
+python -u test.py --gpu 2 --mode 0 --log_KTOT_0.8_edge_2_reduce
+sleep 10
+
+nohup python -u test.py --gpu 0 --mode 0 --log_OT_edge_0_reduce > /dev/null &
+nohup python -u test.py --gpu 1 --mode 0 --log_OT_edge_1_reduce > /dev/null &
+python -u test.py --gpu 2 --mode 0 --log_OT_edge_2_reduce
