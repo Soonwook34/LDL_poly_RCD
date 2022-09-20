bddca95447e6d314a52608817f68e4f9c574b54b
soonwook34 <soonwook34@gmail.com>
2022-09-20 10:31:32 +0000

    complete split data

--- RCD/data/gendata/divide_data_valid_OT.py
+++ RCD/data/gendata/divide_data_valid_OT.py
@@ -4,14 +4,13 @@ import argparse
 import numpy as np
 from scipy.stats import multinomial
 from tqdm import tqdm
+from copy import deepcopy
 
 class DivideDataArgParser(argparse.ArgumentParser):
     def __init__(self):
         super(DivideDataArgParser, self).__init__()
         self.add_argument('--min_log', type=int, default=0,
                           help='Minimum length of logs')
-        self.add_argument('--shuffle', action='store_true',
-                          help='Shuffle log data')
         self.add_argument('--train_ratio', type=float, default=0.8,
                           help='Train set ratio')
         self.add_argument('--valid_ratio', type=float, default=0.1,
@@ -49,50 +48,54 @@ def divide_data(args):
     print(f"loading log_data_{args.name}.json...")
     with open(f"log_data_{args.name}.json", encoding='utf8') as i_f:
         stus = json.load(i_f)
-    # stus_data = []
-    # for stu in stus:
-    #     stus_data.append(stu)
+
+    # 모든 concept에 같은 수의 exercise가 있다는 가정
+    exercise_size = stus[0]['logs'][-1]['exer_id'] + 1
+    concept_size = stus[0]['logs'][-1]['knowledge_code'][0]
+    # 각 concept마다 일정 부분을 train/valid/test set으로 split
+    train_size = int(exercise_size / concept_size * args.train_ratio)
+    valid_size = int(exercise_size / concept_size * args.valid_ratio)
+    test_size = int(exercise_size / concept_size) - train_size - valid_size
     # 데이터셋 나누기
     train_set, valid_set, test_set = [], [], []
     valid_set_reverse, test_set_reverse = [], []
     valid_set_size, test_set_size = 0, 0
     with tqdm(stus, unit="stu") as stus_bar:
         for stu in stus_bar:
-    # for stu in stus:
             user_id = stu['user_id']
             stu_train = {'user_id': user_id}
             stu_valid = {'user_id': user_id}
             stu_test = {'user_id': user_id}
-            train_size = int(stu['log_num'] * args.train_ratio)
-            valid_size = int(stu['log_num'] * args.valid_ratio)
-            test_size = stu['log_num'] - train_size - valid_size
-            valid_set_size += valid_size
-            test_set_size += test_size
+            valid_set_size += int(stu['log_num'] * args.valid_ratio)
+            test_set_size += stu['log_num'] - int(stu['log_num'] * args.train_ratio) - int(stu['log_num'] * args.valid_ratio)
 
-            logs = []
+            logs_by_concept = [[] for c in range(concept_size)]
             for log in stu['logs']:
-                logs.append(log)
-            exercise_size = logs[-1]['exer_id'] + 1
-            concept_size = logs[-1]['knowledge_code'][0]
-            # 데이터셋 전체 셔플 여부
-            if args.shuffle:
-                random.shuffle(logs)
+                logs_by_concept[log['knowledge_code'][0] - 1].append(log)
 
-            # 로그 나누기
-            stu_train['log_num'] = train_size
-            stu_train['logs'] = logs[:train_size]
-            stu_valid['log_num'] = valid_size
-            stu_valid['logs'] = logs[train_size:train_size+valid_size]
-            stu_test['log_num'] = test_size
-            stu_test['logs'] = logs[train_size+valid_size:]
+            stu_train['log_num'] = 0
+            stu_train['logs'] = []
+            stu_valid['log_num'] = 0
+            stu_valid['logs'] = []
+            stu_test['log_num'] = 0
+            stu_test['logs'] = []
+            for logs in logs_by_concept:
+                random.shuffle(logs)
+                # 로그 나누기
+                stu_train['log_num'] += train_size
+                stu_train['logs'] += logs[:train_size]
+                stu_valid['log_num'] += valid_size
+                stu_valid['logs'] += logs[train_size:train_size+valid_size]
+                stu_test['log_num'] += test_size
+                stu_test['logs'] += logs[train_size+valid_size:]
 
             # 나눈 로그 저장
             for log in stu_train['logs']:
                 train_set.append({'user_id': user_id, 'exer_id': log['exer_id'], 'score': log['score'],
                                   'option': log['option'], 'knowledge_code': log['knowledge_code']})
-            valid_set.append(stu_valid)
-            test_set.append(stu_test)
-            # reverse valid/test 데이터
+            valid_set.append(deepcopy(stu_valid))
+            test_set.append(deepcopy(stu_test))
+            # reverse valid/test's option
             for log in stu_valid['logs']:
                 log['option'] = int(np.argmax(multinomial.rvs(n=1, p=reverse(np.array(log["p_e"])))))
                 log['score'] = 1 if log['option'] == log["answer"] else 0
@@ -112,6 +115,14 @@ def divide_data(args):
     print(f"Train {len(train_set)}, Valid {valid_set_size}({len(valid_set)}), Test {test_set_size}({len(test_set)})")
     with open(f"train_set_{args.name}.json", 'w', encoding='utf8') as output_file:
         json.dump(train_set, output_file, indent=4, ensure_ascii=False)
+    for user in valid_set:
+        for log in user['logs']:
+            del log['answer']
+            del log['p_e']
+    for user in test_set:
+        for log in user['logs']:
+            del log['answer']
+            del log['p_e']
     with open(f"valid_set_{args.name}.json", 'w', encoding='utf8') as output_file:
         json.dump(valid_set, output_file, indent=4, ensure_ascii=False)
     with open(f"test_set_{args.name}.json", 'w', encoding='utf8') as output_file:
